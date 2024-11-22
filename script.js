45afd5ee5b564e5dbc4e05a3d640a7cd2e242c95
adriankoder5 <adriankoder5@gmail.com>
2024-11-22 13:41:55 +0100

    idk

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+console.log('Hello :3/');
+// submit button
+const submitbtn = document.querySelector('#submit_btn');
+// personInf
+const nameInp = document.querySelector('#name');
+const surname = document.querySelector('#surname')
+// land
+const land =document.querySelector('#country')
+
+
+function submit(event) {
+    event.preventDefault();
+    const nameS = nameInp.value
+    const surnameS = surname.value
+    const countryS = land.value
+
+    console.log(nameS);console.log(surnameS);console.log(countryS)
+}
+submitbtn.addEventListener('click', submit);
+
