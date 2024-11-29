90753bf220eb3975562f23d76b1af01714445ae2
adriankoder5 <adriankoder5@gmail.com>
2024-11-29 14:35:22 +0100

    finalish

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ console.log('Hello :3/');
 const submitbtn = document.querySelector('#submit_btn');
 // personInf
 const nameInp = document.querySelector('#name');
-const surname = document.querySelector('#surname')
-// land
-const land =document.querySelector('#country')
+const surname = document.querySelector('#surname');
+const land =document.querySelector('#country');
+// const konverteringButton = document.querySelector('#konverter-button')
+
+
 
 
 function submit(event) {
     event.preventDefault();
-    const nameS = nameInp.value
-    const surnameS = surname.value
-    const countryS = land.value
-
-    console.log(nameS);console.log(surnameS);console.log(countryS)
+    const nameS = nameInp.value;
+    const surnameS = surname.value;
+    const countryS = land.value;
+    
+    console.log(nameS);console.log(surnameS);console.log(countryS);
 }
 submitbtn.addEventListener('click', submit);
 
+
+// function konvertering(event2) {
+//     event2.preventDefault();
+//     konverteringButton.textContent = Math.random() * 10000;
+// }
+  
+// konverteringButton.addEventListener("click", konvertering)
